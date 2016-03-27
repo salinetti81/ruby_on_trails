b74e2ec2f5e19880c158e531649f6a7193da699b
Daniel Salinetti <dansalinetti@gmail.com>
2016-03-27 17:26:23 -0400

    Commiting one last time before going home

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,21 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!, only: :secure
+    
     def index
       @users = User.all
     end
 
     def create
+      @user = User.create(user_params)
+      redirect_to users_path
     end
 
     def new
+      @user = User.new
     end
 
     def edit
+      @user = User.find(params[:id])
     end
 
     def show
@@ -18,18 +23,25 @@ class UsersController < ApplicationController
     end
 
     def update
+      @user = User.find(params[:id])
+      @user.update_attributes(user_params)
+      redirect to users_params
     end
 
     def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      redirect to users_path
     end
 
      def secure
       id = current_user.id
       user = User.find(id)
-
-      puts "============"
-      p user
-      puts "============"
     end
 
+    private
+
+    def user_params
+      params.require(:user).permit(:name)
+    end
 end
