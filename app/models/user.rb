9f2159a59490a64edea115d9d5e0103b8064a6a0
Daniel Salinetti <dansalinetti@gmail.com>
2016-03-29 11:35:58 -0400

    Fixing a bug that was preventing new users sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
  	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
  	has_and_belongs_to_many :hikes
- 	validates :first_name, presence: true
- 	validates :last_name, presence: true
- 	validates :about, presence: true
+ 	validates :first_name, presence: false
+ 	validates :last_name, presence: false
+ 	validates :about, presence: false
 end
