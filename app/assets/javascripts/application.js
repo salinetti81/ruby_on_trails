297e7b1ac1a2a15454bea1194d7feeb8f19764a4
Daniel Salinetti <dansalinetti@gmail.com>
2016-04-04 15:35:53 -0400

    Still fixing bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,17 +35,7 @@
 $(document).on("page:change", function() {
   var path = window.location.pathname.split('/')[2]
   console.log(path);
-
-  (function() { // DON'T EDIT BELOW THIS LINE
-  var d = document, s = d.createElement('script');
-
-  s.src = '//hikebuddy.disqus.com/embed.js';
-
-  s.setAttribute('data-timestamp', +new Date());
-  (d.head || d.body).appendChild(s);
-  })();
-  //end of disqus closure
-
+  
   //image uploading by ospry
   var ospry = new Ospry('pk-prod-lcncdvah2hokmvr29hbh5w65');
   console.log(ospry)
@@ -84,35 +74,26 @@ $(document).on("page:change", function() {
       },
     });
   }; // end onupload function
-
   $('#up-form').submit(function(e) {
     e.preventDefault();
     ospry.up({
       form: this,
       imageReady: onUpload,
-
     });
   }); // end up-form
 
 // Slideshow function
 function slideSwitch() {
     var $active = $('#slideshow IMG.active');
-
     if ( $active.length == 0 ) $active = $('#slideshow IMG:last');
-
     // use this to pull the images in the order they appear in the markup
     var $next =  $active.next().length ? $active.next()
         : $('#slideshow IMG:first');
-
     // uncomment the 3 lines below to pull the images in random order
-    
     // var $sibs  = $active.siblings();
     // var rndNum = Math.floor(Math.random() * $sibs.length );
     // var $next  = $( $sibs[ rndNum ] );
-
-
     $active.addClass('last-active');
-
     $next.css({opacity: 0.0})
         .addClass('active')
         .animate({opacity: 1.0}, 1000, function() {
@@ -120,26 +101,15 @@ function slideSwitch() {
         });
 }
 
-// $(function() {
-//     setInterval( "slideSwitch()", 5000 );
-// });
+  (function() { // DON'T EDIT BELOW THIS LINE
+  var d = document, s = d.createElement('script');
 
-// //Instagram API
-//   $.ajax({
-//   type: "GET",
-//   dataType: "jsonp",
-//   cache: false,
-//   url: "https://api.instagram.com/v1/tags/search?q=cambodia/media/recent?client_id=1a612e6037d3494899da939f75b03009&access_token=203001387.1a612e6.ff8c0c44ef8c422296d248e8f5550617"
-//   ,
-//   success: function(data) {
-//   for (var i = 0; i < 10; i++) {
-//   $(".pics").append("<a target='_blank' href='" + data.data[i].link +
-//   "'><img src='" + data.data[i].images.low_resolution.url +"'></img></a>");
-//    }
-//   }
-// });
+  s.src = '//hikebuddy.disqus.com/embed.js';
 
-
+  s.setAttribute('data-timestamp', +new Date());
+  (d.head || d.body).appendChild(s);
+  })();
+  //end of disqus closure
 
 });//ends page change function var map;
 
